c5d770c5ecb0ffbcf98a7a670f70e412cfdfa119
Locke <moyerock@gmail.com>
2018-02-23 12:03:38 +0800

    chore: with vgo

---
+++ go.mod
@@ -0,0 +1,8 @@
+module "github.com/rockdragon/go-practice"
+
+require (
+	"github.com/beego/mux" v0.0.0-20170807094426-6660b4b5accb
+	"go.uber.org/atomic" v1.3.1
+	"go.uber.org/multierr" v1.1.0
+	"go.uber.org/zap" v1.7.1
+)
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"go.uber.org/zap"
 
 	"github.com/beego/mux"
+	"fmt"
 )
 
 func main() {
@@ -13,7 +14,10 @@ func main() {
 	defer logger.Sync()
 	sugar := logger.Sugar()
 
+	serverAddr := "127.0.0.1:8080"
+	fmt.Printf("server running upon: %s\n", serverAddr)
+
 	mx := mux.New()
 	mx.Handler("GET", "/", http.FileServer(http.Dir(".")))
-	sugar.Fatal(http.ListenAndServe("127.0.0.1:8080", mx))
+	sugar.Fatal(http.ListenAndServe(serverAddr, mx))
 }
